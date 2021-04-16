884ae3647fbf7b36bfd1ba38a5f3952d971b2d40
manu314-py <manuelum28@gmail.com>
2021-04-16 14:02:54 +0200

    Tercera version: Modificada clases Hexagono y TestPoligonos, tambien añadido nueva Clase PoligonoRegular

--- Hexagono.java
+++ Hexagono.java
@@ -1,1 +1,9 @@
-//Nueva clase Hexagono
+public class Hexagono extends PoligonoRegular{
+    //Nueva clase Hexagono  
+    private int longitudLado;
+
+    public Hexagono(int numLados, int color, int longitudLado){
+        super(numLados, color);
+        this.longitudLado = longitudLado;
+    }
+}
