efd0786501dce9ee6e53f95aeebff4b409d4daf4
manu314-py <manuelum28@gmail.com>
2021-04-16 14:13:44 +0200

    Cuarta version: Añadido comentario author en la clase Hexagono.java

--- Hexagono.java
+++ Hexagono.java
@@ -1,5 +1,8 @@
 public class Hexagono extends PoligonoRegular{
     //Nueva clase Hexagono  
+    /**
+     * @autor Manuel Urena Masegosa
+     */
     private int longitudLado;
 
     public Hexagono(int numLados, int color, int longitudLado){
